ecace84ea89c08343e8e9fd5ec04e769db4fc5ac
nmbdev <developer.nmb@gmail.com>
2021-11-20 21:43:54 -0500

    Actualización src.js (agregada función obtenerCitasDisponibles)

--- src.js
+++ src.js
@@ -3,47 +3,193 @@ global.CITAS = [
         especialidad: "odontología",
         nombre: "Dr. Juan",
         fecha: "2020-01-01",
-        hora: "7:59",
+        hora: "08:00",
     },
     {
         especialidad: "odontología",
         nombre: "Dr. Juan",
         fecha: "2020-01-01",
-        hora: "9:30",
+        hora: "9:00",
     },
     {
         especialidad: "odontología",
         nombre: "Dr. Juan",
         fecha: "2020-01-01",
-        hora: "12:01",
+        hora: "10:00",
     },
     {
         especialidad: "odontología",
         nombre: "Dr. Juan",
         fecha: "2020-01-01",
-        hora: "12:59",
+        hora: "11:00",
     },
     {
         especialidad: "odontología",
         nombre: "Dr. Juan",
         fecha: "2020-01-01",
-        hora: "19:30",
+        hora: "12:00",
     },
     {
         especialidad: "odontología",
         nombre: "Dr. Juan",
         fecha: "2020-01-01",
-        hora: "14:01",
+        hora: "13:00",
+    },
+    {
+        especialidad: "odontología",
+        nombre: "Dr. Juan",
+        fecha: "2020-01-01",
+        hora: "14:00",
+    },
+    {
+        especialidad: "odontología",
+        nombre: "Dr. Juan",
+        fecha: "2020-01-01",
+        hora: "15:00",
+    },
+    {
+        especialidad: "odontología",
+        nombre: "Dr. Juan",
+        fecha: "2020-01-01",
+        hora: "16:00",
+    },
+    {
+        especialidad: "odontología",
+        nombre: "Dr. Juan",
+        fecha: "2020-01-01",
+        hora: "17:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "08:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "9:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "10:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "11:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "12:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "13:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "14:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "15:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "16:00",
+    },
+    {
+        especialidad: "medicina",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "17:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "8:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "9:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "10:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "11:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "12:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "13:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "14:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "15:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "16:00",
+    },
+    {
+        especialidad: "optometría",
+        nombre: "Dr. Mario",
+        fecha: "2020-01-01",
+        hora: "16:00"
     }
 ];
 
 //===HU-03 - Obtener citas disponibles===
 const obtenerCitasDisponibles = (especialidad, fecha_inicio, fecha_final) => {
-    return obtenerCitasDisponibles.filter(
-        
-        //Código
-
-    );
+    return CITAS.filter(element=> (
+        especialidad == element.especialidad 
+        && 
+        new Date(element.fecha) >= new Date(fecha_inicio)
+        &&
+        new Date(element.fecha) <= new Date(fecha_final)
+    ));
 }
 
 //===HU-04 - Obtener citas por jornada===
@@ -61,8 +207,14 @@ const obtenerCitasPorJornada = (especialidad, fecha_inicio, fecha_final, jornada
 }
 
 //TEST
+//especialidad = "odontología";
+//fecha_inicio = "2020-01-01";
+//fecha_final = "2020-01-01";
+//jornadaPreferida = "tarde";
+
+//console.table(obtenerCitasDisponibles(especialidad, fecha_inicio, fecha_final));
 //console.table(obtenerCitasPorJornada("","","","mañana"));
-//console.table(obtenerCitasPorJornada("","","","tarde"));
+//console.table(obtenerCitasPorJornada(especialidad, fecha_inicio, fecha_final, jornadaPreferida));
 
 module.exports.obtenerCitasDisponibles = obtenerCitasDisponibles;
 module.exports.obtenerCitasPorJornada = obtenerCitasPorJornada;
